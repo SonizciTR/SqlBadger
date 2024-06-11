c890151bd2f1b4afd88a51686745e8281353614f
Ozan Uysal <ozan.uysal@msn.com>
2024-06-11 07:20:46 +0300

    just basic logging added

--- src/app.py
+++ src/app.py
@@ -1,11 +1,24 @@
 import os
+import json 
 
 from services.SqlRawReaderService import SqlRawReaderService
 from services.FileHelper import FileHelper
 
+############
 config_file = f"config.yaml"
+############
+
+def wrt(msg):
+    print(msg)
+
+############
+
+wrt("SqlBadger started.")
 config = FileHelper.read_config(config_file)
+wrt(f"Configs are read : {json.dumps(config.__dict__) }")
 
 file_service = SqlRawReaderService(config)
 
 raw_sql_data = file_service.read_files_recursively()
+
+wrt("SqlBadger finished.")
