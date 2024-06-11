5bae0cfc221bc92cb516c2ad38f71bbdff634687
Ozan Uysal <ozan.uysal@msn.com>
2024-06-11 07:10:42 +0300

    just for testing

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import os
 from services.SqlRawReaderService import SqlRawReaderService
 from services.FileHelper import FileHelper
 
-config_file = f"{os.getcwd()}/src/config.yaml"
+config_file = f"config.yaml"
 config = FileHelper.read_config(config_file)
 
 file_service = SqlRawReaderService(config)
